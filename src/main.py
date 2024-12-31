bda941a959fc7a23b327dbbbff24987e75b2a99b
mgarces <manugp1994@gmail.com>
2024-12-31 12:23:20 +0100

    init repository

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(docs_url="/")
+PORT = 8000
+
+
+@app.get("/test")
+def read_root():
+    return {"message": "¡Hola, mundo!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
+
+def run_server():
+    uvicorn.run("main:app", host="127.0.0.1", port=PORT, reload=True)
+
+
+
+if __name__ == "__main__":
+    run_server()
